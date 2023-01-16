4300f1a26f406d0d2b95a088d292ca346b26e866
devch96 <chjung1006@gmail.com>
2023-01-16 13:59:06 +0900

    loginAPI

--- server/capstone/src/main/java/com/hansung/capstone/controller/LoginController.java
+++ server/capstone/src/main/java/com/hansung/capstone/controller/LoginController.java
@@ -2,22 +2,22 @@ package com.hansung.capstone.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/login")
 public class LoginController {
 
-    @GetMapping("/login")
-    private HttpStatusCode loginCheck(@RequestParam String id, @RequestParam String pwd) {
+    @GetMapping("/check")
+    private ResponseEntity loginCheck(@RequestParam String id, @RequestParam String pwd) {
         String id1 = "1234";
         String pwd1 = "5678";
         if (id.equals(id1) && pwd.equals(pwd1)){
-            return HttpStatus.OK;
+            return new ResponseEntity(HttpStatus.OK);
         } else{
-            return HttpStatus.BAD_REQUEST;
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
     }
+    
 }

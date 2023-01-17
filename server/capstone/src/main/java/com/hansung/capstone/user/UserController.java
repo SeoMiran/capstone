30a119f47537f79c435f8e0e8651a54b698f5ac0
devch96 <chjung1006@gmail.com>
2023-01-17 14:05:23 +0900

    Json Register

--- server/capstone/src/main/java/com/hansung/capstone/user/UserController.java
+++ server/capstone/src/main/java/com/hansung/capstone/user/UserController.java
@@ -35,9 +35,9 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    private ResponseEntity register(UserCreateForm userCreateForm, BindingResult bindingResult){
-        userService.create(userCreateForm.getUsername(),
-                userCreateForm.getPassword1(), userCreateForm.getEmail());
+    private ResponseEntity register(@RequestBody Optional<AppUser> req){
+        AppUser user = req.get();
+        userService.create(user.getUsername(), user.getPassword(), user.getEmail());
         return new ResponseEntity(HttpStatus.OK);
     }
 
